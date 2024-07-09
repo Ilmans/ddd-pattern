abc264256333f01065cf1d5ce9539f78b53ed663
Ilman Sunanuddin <ilmansunannudin2@gmail.com>
2024-07-09 19:27:46 +0700

    translator error and finish register

--- src/Interfaces/http/api/users/handler.js
+++ src/Interfaces/http/api/users/handler.js
@@ -3,12 +3,13 @@ const AddUserUseCase = require("../../../../Applications/use_case/AddUserUseCase
 class UsersHandler {
   constructor(container) {
     this._container = container;
+
     this.postUserHandler = this.postUserHandler.bind(this);
   }
 
-  postUserHandler(request, h) {
+  async postUserHandler(request, h) {
     const addUserUseCase = this._container.getInstance(AddUserUseCase.name);
-    const addedUser = addUserUseCase.execute(request.payload);
+    const addedUser = await addUserUseCase.execute(request.payload);
     const response = h.response({
       status: "success",
       data: {
