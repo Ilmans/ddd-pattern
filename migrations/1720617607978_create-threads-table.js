e316ff02825859c29ca5079ef454cbc02fe92e49
Ilman Sunanuddin <ilmansunannudin2@gmail.com>
2024-07-10 20:28:26 +0700

    create threads and comments migrations

---
+++ migrations/1720617607978_create-threads-table.js
@@ -0,0 +1,42 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+exports.shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.up = (pgm) => {
+  pgm.createTable("threads", {
+    id: { type: "VARCHAR(50)", primaryKey: true },
+    user_id: {
+      type: "varchar(50)", // "integer
+      notNull: true,
+      references: "users",
+      onDelete: "cascade",
+    },
+    title: { type: "varchar(255)", notNull: true },
+    body: { type: "text" },
+    created_at: {
+      type: "timestamp",
+      notNull: true,
+      default: pgm.func("current_timestamp"),
+    },
+    updated_at: {
+      type: "timestamp",
+      notNull: true,
+      default: pgm.func("current_timestamp"),
+    },
+  });
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.down = (pgm) => {
+  pgm.dropTable("threads");
+};
---
+++ migrations/1720617624065_createcommments-table.js
@@ -0,0 +1,65 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+exports.shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.up = (pgm) => {
+  pgm.createTable("comments", {
+    id: { type: "VARCHAR(50)", primaryKey: true },
+    user_id: {
+      type: "varchar(50)",
+      notNull: true,
+      references: "users",
+      onDelete: "cascade",
+    },
+    parent_id: {
+      type: "varchar(50)",
+      references: "comments",
+      onDelete: "cascade",
+      default: null,
+    },
+    thread_id: {
+      type: "varchar(50)",
+      notNull: true,
+      references: "threads",
+      onDelete: "cascade",
+    },
+    body: {
+      type: "text",
+      notNull: true,
+    },
+    //type enum "comment" or "reply"
+    type: {
+      type: "varchar(255)",
+      notNull: true,
+    },
+    created_at: {
+      type: "timestamp",
+      notNull: true,
+      default: pgm.func("current_timestamp"),
+    },
+    updated_at: {
+      type: "timestamp",
+      notNull: true,
+      default: pgm.func("current_timestamp"),
+    },
+    deleted_at: {
+      type: "timestamp",
+      default: null,
+    },
+  });
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.down = (pgm) => {
+  pgm.dropTable("comments");
+};
